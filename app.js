1c1a5bc32eae8a57a3a4a48a0fe712596bbf850f
Esteban Escalante <escalanteaveleyra@gmail.com>
2024-08-12 12:48:17 +0200

    done

--- app.js
+++ app.js
@@ -1,6 +1,40 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
+const express = require("express");
+const morgan = require("morgan");
 
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.json()); 
+app.use(morgan('dev'));
+
+app.get('/',(request,response)=>{
+    response.sendFile(__dirname + '/views/home.html');
+})
+app.get('/blog',(request,response)=>{
+    response.sendFile(__dirname + '/views/blog.html');
+})
+
+
+app.get('/api/projects',(request,response)=>{
+    response.sendFile(__dirname + '/data/projects.json');
+})
+
+app.get('/api/articles',(request,response)=>{
+    response.sendFile(__dirname + '/data/articles.json');
+})
+
+app.get('*',(request,response)=>{
+    response.sendFile(__dirname + '/views/not-found.html');
+})
+
+
+
+app.listen(5005,()=>{
+    console.log("http:/localhost:5005");
+    
+})
 
 
 // CREATE EXPRESS APP
